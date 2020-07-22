9a31f8e26ee0164673f5dd456777d078d8fa8a20
Nicolas Schaller <nicolas_schaller@hotmail.ch@users.noreply.github.com>
2020-07-22 10:10:39 +0200

    initial project. configured to jso responses

---
+++ Animation_Service/WebApplication1/Models/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Country
+    {
+        public string Name { get; set; }
+        public float[] Path { get; set; }
+    }
+}
