f1ecc7925d4584d7c5181ca11b7cd8ee65b4cb8f
Nicolas Schaller <nicolas_schaller@hotmail.ch@users.noreply.github.com>
2020-07-22 23:00:26 +0200

    removed old code

--- Animation_Service/WebApplication1/Services/PersistenceService.cs
+++ Animation_Service/WebApplication1/Services/PersistenceService.cs
@@ -31,57 +31,5 @@ namespace WebApplication1.Services
         {
 
         }
-
-        public string ParseFiles() {
-            var doc = XDocument.Load(Path.Combine(DATA_DIR, COUNTRIES_SRC_FOLDER, FILE_NAME));
-            XNamespace ns = "http://www.opengis.net/kml/2.2";
-
-            // get all outer boundaries. we don't care about the inner ones.
-            var outerBoundaries = doc.Descendants(ns + "outerBoundaryIs").ToList();
-            string countryName = doc.Descendants(ns + "SimpleData").ToList()[0].Value;
-
-            List<string> coordList = new List<string>();
-
-            // list with string for each collection of boarder points
-            foreach (XElement elem in outerBoundaries) {
-                coordList.Add(elem.Element(ns + "LinearRing").Element(ns + "coordinates").Value);
-            }
-
-            char[] spaceSeparator = { ' ' };
-            char[] commaSeparator = { ',' };
-            List<List<Dictionary<string, double>>> countryCoordinates = new List<List<Dictionary<string, double>>>();
-            foreach (string coords in coordList) {
-                String[] latLongPairList = coords.Split(spaceSeparator, StringSplitOptions.RemoveEmptyEntries);
-                List<Dictionary<string, double>> coordPoints = new List<Dictionary<string, double>>();
-                foreach (string latLongPair in latLongPairList) {
-                    String[] latLongValues = latLongPair.Split(commaSeparator, StringSplitOptions.RemoveEmptyEntries);
-                    Dictionary<string, double> latLong = new Dictionary<string, double>();
-                    latLong.Add("lng", Convert.ToDouble(latLongValues[0]));
-                    latLong.Add("lat", Convert.ToDouble(latLongValues[1]));
-                    coordPoints.Add(latLong);
-                }
-                countryCoordinates.Add(coordPoints);
-            }
-
-            Dictionary<string, Object> country = new Dictionary<string, Object>();
-            country.Add("name", countryName);
-            country.Add("coordinates", countryCoordinates);
-
-            var destinationPath = Path.Combine(DATA_DIR, ANIMATIONS_STORAGE_FOLDER);
-
-            if (!Directory.Exists(destinationPath))
-            {
-                Directory.CreateDirectory(destinationPath);
-            }
-
-            using (StreamWriter file = File.CreateText(Path.Combine(destinationPath, FILE_NAME + ".json")))
-            {
-                JsonSerializer serializer = new JsonSerializer();
-                //serialize object directly into file stream
-                serializer.Serialize(file, country);
-            }
-
-            return "";
-        }
     }
 }
